b7961b9d0c4c08b1f505d52d3be133e31c2125f9
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-08-19 22:13:40 +0200

    Now using gapuino config

--- cmake/emu/pulp.cmake
+++ cmake/emu/pulp.cmake
@@ -2,7 +2,7 @@ separate_arguments(runner_args WINDOWS_COMMAND "$ENV{RUNNER_ARGS}")
 
 add_custom_target(run
   COMMAND
-  pulp-run --config=gap --binary=${APPLICATION_BINARY_DIR}/zephyr/${KERNEL_ELF_NAME} prepare run ${runner_args}
+  pulp-run --config=gapuino --binary=${APPLICATION_BINARY_DIR}/zephyr/${KERNEL_ELF_NAME} prepare run ${runner_args}
   DEPENDS ${logical_target_for_zephyr_elf}
   WORKING_DIRECTORY ${APPLICATION_BINARY_DIR}
   USES_TERMINAL
